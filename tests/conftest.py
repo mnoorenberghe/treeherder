2e7fc8f76be02810c14e09f974ca9125b917002c
James Graham <james@hoppipolla.co.uk>
2015-11-16 13:17:50 +0000

    Add a setting to disable autoclassification of jobs when all lines are autoclassified

--- tests/conftest.py
+++ tests/conftest.py
@@ -501,3 +501,9 @@ def test_perf_signature(test_repository):
         test='mytest'
     )
     return signature
+
+
+@pytest.fixture
+def mock_autoclassify_jobs_true(monkeypatch):
+    from django.conf import settings
+    monkeypatch.setattr(settings, 'AUTOCLASSIFY_JOBS', True)
