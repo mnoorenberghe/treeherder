341314e81a3402ea104792ca7c0b22e30ce2bc89
Ed Morley <emorley@mozilla.com>
2016-01-03 01:33:33 +0000

    Merge conflicting migrations after rebase on master
    Generated by running `manage.py makemigrations --merge`.
    Once this migration has been run on Heroku, we can squash these down
    and use a `migrate --fake` on Heroku to tidy up before we merge this to
    master.

---
+++ treeherder/model/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('model', '0007_auto_20151203_0458'),
+        ('model', '0007_remove_datasource_oauth_fields'),
+    ]
+
+    operations = [
+    ]
